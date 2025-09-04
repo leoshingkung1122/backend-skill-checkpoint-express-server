7ae9031d25d90f6da152fd1e130228bc25e02d8f
Shin <sirapop1998@gmail.com>
2025-09-04 09:57:05 +0700

    test: test database connection with select all

--- app.mjs
+++ app.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4000;
@@ -8,6 +9,16 @@ app.use(express.json());
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
+
+app.get("/test2", async (req, res) => {
+  try {
+    const result = await connectionPool.query("SELECT count(*) FROM public.answer_votes");
+    return res.status(200).json(result.rows[0]);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
